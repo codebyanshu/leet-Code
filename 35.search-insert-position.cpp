0f135a217da523ad36d455a0a85fca497ae5c05e
hackeranshu007 <anshu290290@gmail.com>
2025-09-30 21:22:58 +0530

    35 Corrected

--- 35.search-insert-position.cpp
+++ 35.search-insert-position.cpp
@@ -5,10 +5,12 @@
  */
 
 // @lc code=start
-class Solution {
-public:
 
-    int searchInsert(vector<int>& nums, int target) {
+class Solution
+{
+public:
+    int searchInsert(vector<int> &nums, int target)
+    {
         int low = 0;
         int high = nums.size() - 1;
         int mid = low + (high - low) / 2;
@@ -29,8 +31,9 @@ public:
                 high = mid - 1;
             }
         }
-        return mid;
+        return low;
     }
 };
+
 // @lc code=end
 
